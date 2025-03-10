c68bdccd4c9c1ab1b790377d8d00c925411b0d7a
imjakir <36418218+imjakir@users.noreply.github.com>
2025-03-10 23:49:04 +0530

    added retries: 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-
+  retries: 1,
   reporter: 'line',
   use: {
     browserName: "chromium",
